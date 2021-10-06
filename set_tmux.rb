5bad815ceecaf46d3cbacf1c11d8c5d726f069ce
Marcio Pereira <marcio@gaspardbruno.com>
2021-10-06 19:33:14 +0100

    added tmux conf and helper for init.

---
+++ set_tmux.rb
@@ -0,0 +1,27 @@
+require 'json'
+
+def send_tmux_command(ref, cmd)
+ system("tmux send-keys -t #{ref} \"#{cmd}\" ENTER")
+end
+
+file = File.read('./tmux.dev.conf.json')
+
+conf_tmux = JSON.parse(file)
+
+sessions = conf_tmux['sessions']
+
+sessions.each_with_index do |session, session_index|
+  system("tmux new-session -d -s #{session['name']}")
+  
+  windows = session['windows']
+
+  windows.each_with_index do |window, window_index| 
+    ref = "#{session['name']}:#{window_index + 1}"
+
+    system("tmux new-window -t #{ref} -n #{window['name']}")
+    send_tmux_command ref, "cd #{window['path']}"
+    send_tmux_command ref, "clear"
+  end
+end
+
+system("tmux a")
