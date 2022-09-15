46042e551dc9a8bb7f32374ffb121b40d84b82ca
mp <mp.262gr@slmail.me>
2022-09-15 21:46:46 +0100

    tab ind fix.

--- dotfiles/.config/nvim/test.ts
+++ dotfiles/.config/nvim/test.ts
@@ -1,6 +1,6 @@
 function foobar() {
-    const foobar = {bar: "val", derp: "val2"}
-    const bar = [1, 2, 3]
+  const foobar = {bar: "val", derp: "val2"}
+  const bar = [1, 2, 3]
 
-    return foobar
+  return foobar
 }
