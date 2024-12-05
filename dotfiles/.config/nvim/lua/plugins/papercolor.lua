a4f7bef359570ae911b7d20fd08d7b1944a48ec2
Marcio Pereira <marcio.rivais@gmail.com>
2024-12-05 16:22:27 +0000

    changed nvim conf, using lazy packager.

---
+++ dotfiles/.config/nvim/lua/plugins/papercolor.lua
@@ -0,0 +1,8 @@
+return {
+  'NLKNguyen/papercolor-theme',
+  lazy = false,
+  priority = 1000,
+  config = function() 
+    vim.cmd.colorscheme "PaperColor"
+  end
+}
--- dotfiles/.config/nvim/test.js
+++ dotfiles/.config/nvim/test.js
@@ -1,5 +1,7 @@
 function foobar() {
+  var foobar1 = "test";
   const foobar = {bar: "val", derp: "val2"}
+
   const bar = [1, 2, 3]
 
   return foobar
